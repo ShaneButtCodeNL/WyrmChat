d94e1577854751a86268fe43252d0064f491ae06
shane butt <shanebuttcode@gmail.com>
2022-04-23 11:32:45 -0230

    Initial setup

---
+++ Models/UserMessage.cs
@@ -0,0 +1,11 @@
+
+
+namespace WyrmChat.Models;
+public class UserMessage{
+   public string UserName { get; set; }
+   public string Message { get; set; }
+   public bool CurrentUser { get; set; }
+   public DateTime DateSent { get; set; }
+
+
+}
